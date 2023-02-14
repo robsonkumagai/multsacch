e8d4a944a84995e6bc418512182eadd7b1fe2a4f
Robson Kumagai <robsonkumagai@gmail.com>
2023-02-14 09:21:29 -0300

    Changed colors

--- src/theme/index.js
+++ src/theme/index.js
@@ -8,8 +8,8 @@ export default {
     background: '#FFFFFF', // body background color
     background_secondary: '#F9FBFD', // secondary background color
     border_color: '#E5ECF4', // border color
-    primary: '#1F3E76', // primary button and link color
-    secondary: '#25CB9E', // secondary color - can be used for hover states
+    primary: '#8a1d86', // primary button and link color
+    secondary: '#8a1d86', // secondary color - can be used for hover states
     muted: '#7B8188', // muted color
     accent: '#609', // a contrast color for emphasizing UI
     yellow: '#F6C416',
