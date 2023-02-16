43bc1c486d52811959147dd3223b5c956d97f35b
Robson Kumagai <robsonkumagai@gmail.com>
2023-02-16 16:43:23 -0300

    Add animate import

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -8,6 +8,7 @@ import 'typeface-dm-sans';
 import 'react-modal-video/css/modal-video.min.css';
 import 'rc-drawer/assets/index.css';
 import "react-responsive-carousel/lib/styles/carousel.min.css";
+import "animate.css/animate.min.css";
 
 export default function CustomApp({ Component, pageProps }) {
   useEffect(() => {
