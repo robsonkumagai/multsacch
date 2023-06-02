a8725f3ebf9346a4e0d1a929d057f5f4f3e10083
Robson Kumagai <robsonkumagai@gmail.com>
2023-06-02 10:22:55 -0300

    Change app seo

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,6 @@
 import { useEffect } from 'react';
 import { initGA, logPageView } from '../../utils/analytics';
-import { NextSeo, DefaultSeo } from 'next-seo';
+import { ArticleJsonLd, NextSeo, DefaultSeo } from 'next-seo';
 
 // Load DM Sans typeface
 import 'typeface-dm-sans';
@@ -21,22 +21,23 @@ export default function CustomApp({ Component, pageProps }) {
     logPageView();
   }, []);
 
-  const defaultSeoConfig = {
-    title: 'MultSacch - Aditivo probiótico para ruminantes',
-    description: 'MultSacch - Com cepas exclusivas e selecionadas MultSacch é a melhor opção de aditivo probiótico para bovinos, bubalinos, ovinos e caprinos. Entre em contato e solicite uma cotação.',
-    openGraph: {
-      type: 'website',
-      url: 'https://www.multsacch.com',
-      title: 'MultSacch - Aditivo probiótico para ruminantes',
-      description: 'MultSacch - Com cepas exclusivas e selecionadas MultSacch é a melhor opção de aditivo probiótico para bovinos, bubalinos, ovinos e caprinos. Entre em contato e solicite uma cotação.',
-      images: [
-        {
-          url: 'https://www.multsacch.com/_next/static/images/mockup-ec874ec14c52d48144db66ea47595a72.webp',
-          alt: 'Embalagem do produto MultSacch',
-        },
-      ],
-    },
-  };
+  // const defaultSeoConfig = {
+  //   title: 'MultSacch - Aditivo probiótico para ruminantes',
+  //   description: 'MultSacch - Com cepas exclusivas e selecionadas MultSacch é a melhor opção de aditivo probiótico para bovinos, bubalinos, ovinos e caprinos. Entre em contato e solicite uma cotação.',
+  //   canonical: 'https://www.multsacch.com',
+  //   openGraph: {
+  //     type: 'website',
+  //     url: 'https://www.multsacch.com',
+  //     title: 'MultSacch - Aditivo probiótico para ruminantes',
+  //     description: 'MultSacch - Com cepas exclusivas e selecionadas MultSacch é a melhor opção de aditivo probiótico para bovinos, bubalinos, ovinos e caprinos. Entre em contato e solicite uma cotação.',
+  //     images: [
+  //       {
+  //         url: 'https://www.multsacch.com/_next/static/images/mockup-ec874ec14c52d48144db66ea47595a72.webp',
+  //         alt: 'Embalagem do produto MultSacch',
+  //       },
+  //     ],
+  //   },
+  // };
 
   const structuredData = {
     '@context': 'https://schema.org',
@@ -48,13 +49,37 @@ export default function CustomApp({ Component, pageProps }) {
 
   return (
     <>
-      <DefaultSeo {...defaultSeoConfig} />
       <NextSeo
         title="MultSacch - Aditivo probiótico para ruminantes"
         description="MultSacch - Com cepas exclusivas e selecionadas MultSacch é a melhor opção de aditivo probiótico para bovinos, bubalinos, ovinos e caprinos. Entre em contato e solicite uma cotação."
-        jsonLd={[structuredData]}
-
-      // Adicione outras informações de SEO personalizadas, se necessário
+        canonical='https://www.multsacch.com'
+        openGraph={{
+          type: 'website',
+          url: 'https://www.multsacch.com',
+          title: 'MultSacch - Aditivo probiótico para ruminantes',
+          description: 'MultSacch - Com cepas exclusivas e selecionadas MultSacch é a melhor opção de aditivo probiótico para bovinos, bubalinos, ovinos e caprinos. Entre em contato e solicite uma cotação.',
+          images: [
+            {
+              url: 'https://www.multsacch.com/_next/static/images/mockup-ec874ec14c52d48144db66ea47595a72.webp',
+              alt: 'Embalagem do produto MultSacch',
+            },
+          ],
+        }}
+      />
+      <ArticleJsonLd
+        type="website"
+        url="https://www.multsacch.com"
+        title="MultSacch - Aditivo probiótico para ruminantes"
+        images={[
+          {
+            url: 'https://www.multsacch.com/_next/static/images/mockup-ec874ec14c52d48144db66ea47595a72.webp',
+            alt: 'Embalagem do produto MultSacch',
+          },
+        ]}
+        datePublished="2023-05-11T23:04:13Z"
+        dateModified="2023-06-02T09:50:13Z"
+        authorName="Biomart Nutrição Animal Imp. e Exp LTDA"
+        description="MultSacch - Com cepas exclusivas e selecionadas MultSacch é a melhor opção de aditivo probiótico para bovinos, bubalinos, ovinos e caprinos. Entre em contato e solicite uma cotação."
       />
       <Component {...pageProps} />
     </>
